1bc7af3bcb202c802488ac0a88f384e82d042cd2
Moses Hunsu <hunsumoses7@gmail.com>
2023-05-02 14:47:27 +0100

    Checkbox

--- src/JavaFx05CheckBox/Main.java
+++ src/JavaFx05CheckBox/Main.java
@@ -1,6 +1,13 @@
 package JavaFx05CheckBox;
+
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.Objects;
 
 public class Main extends Application {
 
@@ -9,7 +16,13 @@ public class Main extends Application {
     }
 
     @Override
-    public void start(Stage primaryStage) {
-        
+    public void start(Stage stage) throws IOException {
+
+        Parent root = FXMLLoader.load(Objects.requireNonNull(getClass().getResource("sample.fxml")));
+
+        Scene scene = new Scene(root, 300, 300);
+        stage.setScene(scene);
+        stage.setTitle("Check Buttons");
+        stage.show();
     }
 }
--- src/JavaFx05CheckBox/SampleController.java
+++ src/JavaFx05CheckBox/SampleController.java
@@ -1,2 +1,37 @@
-package JavaFx05CheckBox;public class SampleController {
+package JavaFx05CheckBox;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.TextArea;
+
+public class SampleController {
+    @FXML
+    private CheckBox chkApple;
+    @FXML
+    private CheckBox chkOrange;
+    @FXML
+    private CheckBox chkPear;
+    @FXML
+    private Button btnOK;
+    @FXML
+    private TextArea taSummary;
+
+    public void handleBtnOk(ActionEvent actionEvent) {
+        // convert boolean values to string values
+        taSummary.appendText(String.valueOf(chkApple.isSelected() + "\n"));
+        taSummary.appendText(String.valueOf(chkOrange.isSelected() + "\n"));
+        taSummary.appendText(String.valueOf(chkPear.isSelected() + "\n"));
+
+        if (chkApple.isSelected()) {
+            taSummary.appendText(chkApple.getText() + " $1.20 per kg \n");
+        }
+        if (chkOrange.isSelected()) {
+            taSummary.appendText(chkOrange.getText() + " $1.00 per kg \n");
+        }
+        if (chkPear.isSelected()) {
+            taSummary.appendText(chkPear.getText() + " $1.50 per kg \n");
+        }
+    }
 }
