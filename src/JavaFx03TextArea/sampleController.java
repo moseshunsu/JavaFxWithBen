16ce0eecb51f1d478d2249f59d4f56a51f072522
Moses Hunsu <hunsumoses7@gmail.com>
2023-05-02 14:44:16 +0100

    Text Area

--- src/JavaFx03TextArea/sampleController.java
+++ src/JavaFx03TextArea/sampleController.java
@@ -1,2 +1,21 @@
+package JavaFx03TextArea;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+
 public class sampleController {
+    @FXML
+    private TextArea taComments;
+    @FXML
+    private TextField tfComments;
+    @FXML
+    private Button btnOk;
+
+    public void handleBtnOk(ActionEvent actionEvent) {
+//        taComments.setText(tfComments.getText());
+        taComments.appendText(tfComments.getText() + "\n");
+    }
 }
