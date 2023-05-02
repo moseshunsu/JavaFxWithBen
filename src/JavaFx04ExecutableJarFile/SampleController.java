d0616c30c903d44d7fff9d27c267cf132df2a42d
Moses Hunsu <hunsumoses7@gmail.com>
2023-05-02 14:46:02 +0100

    Executable Jar File

--- src/JavaFx04ExecutableJarFile/SampleController.java
+++ src/JavaFx04ExecutableJarFile/SampleController.java
@@ -1,2 +1,17 @@
-package JavaFx04ExecutableJarFile;public class SampleController {
+package JavaFx04ExecutableJarFile;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+
+public class SampleController {
+    @FXML
+    private Label lblMessage;
+    @FXML
+    private Button btnOk;
+
+    public void handleBtnOk(ActionEvent actionEvent) {
+        lblMessage.setText("OK button is clicked");
+    }
 }
