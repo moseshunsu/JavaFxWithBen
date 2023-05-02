82e0e4696b894a8f53a28a816e3c838053c85c73
Moses Hunsu <hunsumoses7@gmail.com>
2023-05-02 14:48:09 +0100

    Additional commits

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,28 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
-public class Main {
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        launch(args);
+    }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    @Override
+    public void start(Stage stage) throws Exception {
+        StackPane layout = new StackPane();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        Button button = new Button("Hello World");
+
+        button.setOnAction(actionEvent -> {
+            System.out.println(System.getProperty("java.version"));
+        });
+
+        layout.getChildren().add(button);
+        Scene scene = new Scene(layout, 300, 300);
+        stage.setScene(scene);
+        stage.setTitle("JavaFx 19");
+        stage.show();
     }
 }
